d2ce7ad9f7720e9a683830f2e296a66c2bc04005
Andriy Paliychuk <a.b.paliychuk@gmail.com>
2025-01-10 08:18:38 +0200

    add

--- src/main/java/balabux/production/BinarySearch.java
+++ src/main/java/balabux/production/BinarySearch.java
@@ -1,35 +1,35 @@
 package balabux.production;
 
 public class BinarySearch {
-    int binarySearch(int arr[], int x) {
-        int l = 0, r = arr.length - 1;
-        while (l <= r) {
-            int m = l + (r - l) / 2;
+    int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
 
             // Check if x is present at mid
-            if (arr[m] == x) return m;
+            if (arr[middle] == target) return middle;
 
-            // If x greater, ignore left half
-            if (arr[m] < x) {
-                l = m + 1;
+            if (arr[middle] < target) { // If x greater, ignore left half
+                left = middle + 1;
             } else { // If x is smaller, ignore right half
-                r = m - 1;
+                right = middle - 1;
             }
         }
         // If we reach here, then element was not present
         return -1;
     }
 
-    // Driver code
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         BinarySearch ob = new BinarySearch();
-        int arr[] = { 2, 3, 4, 10, 40 };
-        int x = 10;
-        int result = ob.binarySearch(arr, x);
+        int[] arr = { 2, 3, 4, 10, 40 };
+        int target = 10;
+        int result = ob.binarySearch(arr, target);
         if (result == -1) {
             System.out.println("Element is not present in array");
         } else {
             System.out.println("Element is present at index " + result);
         }
     }
+
 }
---
+++ src/main/java/balabux/production/MergeSortDemo.java
@@ -0,0 +1,48 @@
+package balabux.production;
+
+import java.util.Arrays;
+
+public class MergeSortDemo {
+
+    void sort(int[] array, int left, int right) {
+        if (right > left) {
+            int middle = left + (right - left) / 2;
+            sort(array, left, middle);
+            sort(array, middle + 1, right);
+            merge(array, left, middle, right);
+        }
+    }
+
+    void merge(int[] array, int left, int middle, int right) {
+        int[] leftSubArray = new int[middle - left + 1];
+        int[] rightSubArray = new int[right - middle];
+
+        System.arraycopy(array, left, leftSubArray, 0, leftSubArray.length);
+        System.arraycopy(array, middle + 1, rightSubArray, 0, rightSubArray.length);
+
+        int leftIndex = 0;
+        int rightIndex = 0;
+        int mergeIndex = left;
+
+        while(leftIndex < leftSubArray.length && rightIndex < rightSubArray.length) {
+            if (leftSubArray[leftIndex] > rightSubArray[rightIndex]) {
+                array[mergeIndex] = rightSubArray[rightIndex++];
+            } else {
+                array[mergeIndex] = leftSubArray[leftIndex++];
+            }
+            mergeIndex++;
+        }
+
+        System.arraycopy(leftSubArray, leftIndex, array, mergeIndex, leftSubArray.length - leftIndex);
+        System.arraycopy(rightSubArray, rightIndex, array, mergeIndex, rightSubArray.length - rightIndex);
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1,8,9,4,3,5,5,6,5,1,2,50,46};
+        MergeSortDemo mergeSort = new MergeSortDemo();
+
+        System.out.println(Arrays.toString(array));
+        mergeSort.sort(array, 0, array.length - 1);
+        System.out.println(Arrays.toString(array));
+    }
+}
